ad738f9e3834636ce3d6563ede048f28b7a205f3
sunyeyu <869225586@qq.com>
2020-08-11 16:00:18 +0800

    调整个人中心的

--- lib/pages/PersonPage.dart
+++ lib/pages/PersonPage.dart
@@ -1,49 +1,59 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class PersonPage extends StatefulWidget{
+class PersonPage extends StatefulWidget {
   @override
-  _PersonState createState()=>_PersonState();
-
+  _PersonState createState() => _PersonState();
 }
 
-class _PersonState<PersonPage> extends State{
+class _PersonState<PersonPage> extends State {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Column(
-        children: <Widget>[
-          Center(
-
-            child: Image.network("https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
+      children: <Widget>[
+        Center(
+          child: Image.network(
+              "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
               width: 64.0,
               height: 64.0),
-          ),
-
-          Flexible(
-              child:ListView.builder(
-             itemCount: 3,
-             itemExtent: 50,//强制条目高度
-            itemBuilder:(BuildContext context, int index) {
-               switch(index){
-                 case 0:
-                   return Text("姓名：茨木童子",style:TextStyle(color:Colors.orange),
-                   textAlign: TextAlign.left,);
-                   break;
-                 case 1:
-                   return Text("技能：一拳超人 地狱右手",style:TextStyle(color:Colors.orange),
-                     textAlign: TextAlign.left,);
-                   break;
-                 case 2:
-                   return Text("好基友：鬼吞童子",style:TextStyle(color:Colors.orange),
-                     textAlign: TextAlign.left,);;
-                   break;
-               }
-
-              }
-          ))
-        ],
+        ),
+        Flexible(
+            child: ListView.builder(
+                itemCount: 3,
+                itemExtent: 50, //强制条目高度
+                itemBuilder: (BuildContext context, int index) {
+                  switch (index) {
+                    case 0:
+                      return Padding(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Text(
+                            "姓名：茨木童子",
+                            style: TextStyle(color: Colors.orange),
+                            textAlign: TextAlign.left,
+                          ));
+                      break;
+                    case 1:
+                      return Padding(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Text(
+                            "技能：一拳超人 地狱右手",
+                            style: TextStyle(color: Colors.orange),
+                            textAlign: TextAlign.left,
+                          ));
+                      break;
+                    case 2:
+                      return Padding(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Text(
+                            "好基友：鬼吞童子",
+                            style: TextStyle(color: Colors.orange),
+                            textAlign: TextAlign.left,
+                          ));
+                      break;
+                  }
+                }))
+      ],
     );
   }
-
 }
